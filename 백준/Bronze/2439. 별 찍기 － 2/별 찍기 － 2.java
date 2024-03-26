1fc8ce12452753f0018259b9502d8a6fe64cb75e
미지 <112713361+Jimmy0613@users.noreply.github.com>
2024-03-26 23:30:07 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i<=n; i++) {
+            for (int j = n-i; j > 0; j--) {
+                bw.write(" ");
+            }
+            for(int k = 0; k < i; k++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    } 
+}
