df289a7c03d4ae801324eb4cc0a1fbe9963f548e
미지 <112713361+Jimmy0613@users.noreply.github.com>
2025-08-05 11:32:26 +0900

    [Bronze III] Title: 직각삼각형, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a, b, c;
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");    
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            
+            if (a == 0 && b == 0 && c == 0) break;
+    
+            int max = Math.max(a, Math.max(b, c));
+            int sum = a * a + b * b + c * c - max * max;
+
+            String answer = (max * max == sum) ? "right" : "wrong";
+    
+            bw.write(answer + "\n");
+        }
+
+        
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
